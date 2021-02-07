0470d44b7a022bb119be14fd51179b7f6c4a35a7
nkuninti <nishanthreddy_kuninti@intuit.com>
2021-02-07 14:38:58 -0800

    present crash alert

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -2,18 +2,38 @@
 //  ViewController.swift
 //  RetirementCalculator
 //
-//  Created by Nishanth Reddy Kuninti on 2/7/21.
+//  Created by Eduardo Rosas on 8/19/19.
+//  Copyright © 2019 Eduardo Rosas. All rights reserved.
 //
 
 import UIKit
+import AppCenterCrashes
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+        Crashes.generateTestCrash()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "crashed", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool!", style: .default, handler: nil))
+            present(alert, animated: true)
+        }
+    }
+    
 }
 
